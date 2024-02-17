286666b70ff36488f3470f1f9e5f35c0d56e80f4
seda savaş <sedanursavas@tregitim.com.tr>
2024-02-17 19:29:34 +0300

    isPowerOn verisini sharedpreferences ile lokalde tutma kısmı eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_devices/view-models/device_view_model.dart';
 import 'package:smart_devices/view/home_screen.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences _prefs = await SharedPreferences.getInstance();
+  final DeviceViewModel deviceViewModel = DeviceViewModel(_prefs);
+  deviceViewModel.loadDeviceData();
+  runApp(ChangeNotifierProvider(
+    create: (context) => deviceViewModel,
+    child: const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => DeviceViewModel(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
-      ),
-    );
-  }
-}
--- lib/view-models/device_view_model.dart
+++ lib/view-models/device_view_model.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_devices/models/device.dart';
 
 class DeviceViewModel extends ChangeNotifier {
+  late SharedPreferences _prefs;
+
   List<Device> devices = [
     Device(name: 'Fan', isPowerOn: false, icon: Icons.wind_power),
     Device(name: 'Temperature', isPowerOn: false, icon: Icons.thermostat),
   ];
 
+  DeviceViewModel(this._prefs) {
+    loadDeviceData();
+  }
+
+  void loadDeviceData() async {
+    _prefs = await SharedPreferences.getInstance();
+    for (int i = 0; i < devices.length; i++) {
+      devices[i].isPowerOn = _prefs.getBool('device_$i') ?? false;
+    }
+    notifyListeners();
+  }
+
+  void _saveDeviceData() async {
+    for (int i = 0; i < devices.length; i++) {
+      await _prefs.setBool('device_$i', devices[i].isPowerOn);
+    }
+  }
+
   void devicePowerSwitch(bool value, int index) {
     devices[index].isPowerOn = value;
+    _saveDeviceData();
     notifyListeners();
   }
 }
